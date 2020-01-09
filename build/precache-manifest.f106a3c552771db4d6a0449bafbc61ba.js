7a27d612c49a90d3b517173e928701eee3db4afe
PW <przemek.wrobel@poczta.fm>
2020-01-09 20:26:01 +0100

    build

---
+++ build/precache-manifest.f106a3c552771db4d6a0449bafbc61ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad87f6f3fd8000600ce53ad13d40db4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb54822fd6e8c7a4cd1c",
+    "url": "/static/css/main.700fc2c8.chunk.css"
+  },
+  {
+    "revision": "7d776cdb4c190244f536",
+    "url": "/static/js/2.3bec868e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3bec868e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb54822fd6e8c7a4cd1c",
+    "url": "/static/js/main.074cfb9f.chunk.js"
+  },
+  {
+    "revision": "15a60ad5ed6b7d39658e",
+    "url": "/static/js/runtime-main.9fb7a550.js"
+  }
+]);
